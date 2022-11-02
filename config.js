9ad10557ec12e31bc63762d1f4ea9ee8d6a77003
gilzzbotz <104905739+gilzzbotz@users.noreply.github.com>
2022-11-02 18:57:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
-global.you = ['6285850539404'] 
+global.you = ['6285730794089'] 
 //Developer Bot
-global.gc = 'https://chat.whatsapp.com/I5Kz3Abawc5DqyBSDoYvKB'
-global.rowner = ['6285850539404'] // Real Owner Bot
-global.owner = ['6285850539404'] // Owner Bot
-global.police = ['6285850539404'] // Police Bot
-global.mods = ['6285850539404']  // Want some help?
-global.prems = ['6285850539404'] // Premium user has unlimited limit
-global.fotonya1 = 'https://telegra.ph/file/00ca947f49354f6c108da.jpg' //ini kalo mau di ganti silahkan ganti aja
-global.fotonya2 = 'https://telegra.ph/file/00ca947f49354f6c108da.jpg' //gw lupa nih foto apa aja:v
-global.fotonya3 = 'https://telegra.ph/file/00ca947f49354f6c108da.jpg' //ini juga ganti
-global.fotonya4 = 'https://telegra.ph/file/00ca947f49354f6c108da.jpg' //ini juga ganti
-global.fotonya5 = 'https://telegra.ph/file/00ca947f49354f6c108da.jpg' //ini juga ganti
-global.fotonya6 = 'https://telegra.ph/file/00ca947f49354f6c108da.jpg' //ini ganti pakai foto sosialmedia mu
-global.fotonya7 = 'https://telegra.ph/file/00ca947f49354f6c108da.jpg'
-global.instagram = 'https://instagram.com/Jarotr_'
+global.gc = 'https://chat.whatsapp.com/CxJShadfxYqCVAHHxdHSiy'
+global.rowner = ['6285730794089'] // Real Owner Bot
+global.owner = ['6285730794089'] // Owner Bot
+global.police = ['6285730794089'] // Police Bot
+global.mods = ['6285730794089']  // Want some help?
+global.prems = ['6285730794089'] // Premium user has unlimited limit
+global.fotonya1 = 'https://telegra.ph/file/7d10aa32a62f33efffa47.jpg' //ini kalo mau di ganti silahkan ganti aja
+global.fotonya2 = 'https://telegra.ph/file/7d10aa32a62f33efffa47.jpg' //gw lupa nih foto apa aja:v
+global.fotonya3 = 'https://telegra.ph/file/7d10aa32a62f33efffa47.jpg' //ini juga ganti
+global.fotonya4 = 'https://telegra.ph/file/7d10aa32a62f33efffa47.jpg' //ini juga ganti
+global.fotonya5 = 'https://telegra.ph/file/7d10aa32a62f33efffa47.jpg' //ini juga ganti
+global.fotonya6 = 'https://telegra.ph/file/7d10aa32a62f33efffa47.jpg' //ini ganti pakai foto sosialmedia mu
+global.fotonya7 = 'https://telegra.ph/file/7d10aa32a62f33efffa47.jpg'
+global.instagram = 'https://instagram.com/rdtyzagilz'
 
 global.helper = ['6285850539404']
 global.APIs = { // API Prefix
@@ -83,9 +83,9 @@ global.fsizedoc = '564588643' // default 10TB
 global.fpagedoc = '444'
 
 // Sticker WM
-global.packname = 'Jarot offc'
-global.author = 'Jarot offc'
-global.wm = '© Jarot offc'
+global.packname = 'Gilzz offc'
+global.author = 'Gilzz offc'
+global.wm = '© Gilzz offc'
 
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
 
